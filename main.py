3e8aa778a23fa26d9eaf142d0339cb16692560f6
Dan Thompson <danielrthompsonjr@gmail.com>
2022-07-24 09:33:25 -0400

    deta and cron working!

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from deta import App
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 # from pydantic import BaseModel
 
 
-app = FastAPI()
+app = App(FastAPI())
 
 
-@app.get("/")
+@app.get("/", response_class=JSONResponse)
 async def root():
     return {"message": "Hello World"}
+
+
+@app.lib.cron()
+def cron_job(event):
+    return "Hello Deta, I am a cron job"
