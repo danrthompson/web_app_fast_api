da74940bcf1a5d9f6d2ed08734aeae27b96a1543
Dan Thompson <danielrthompsonjr@gmail.com>
2023-03-22 16:13:45 -0400

    updating fast api web app to current state

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from deta import App
+# from deta import App
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
+
+# from fastapi.responses import JSONResponse
 
 # from pydantic import BaseModel
 
 
-app = App(FastAPI())
+# app = App(FastAPI())
+app = FastAPI()
 
 
-@app.get("/", response_class=JSONResponse)
+# @app.get("/", response_class=JSONResponse)
+@app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.lib.cron()
-def cron_job(event):
-    return "Hello Deta, I am a cron job"
+# @app.lib.cron()
+# def cron_job(event):
+#     return "Hello Deta, I am a cron job"
